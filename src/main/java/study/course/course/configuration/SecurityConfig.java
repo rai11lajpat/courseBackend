a22e6293cf19d859fbffa41c602907aa02e7e607
Lajpat Rai <lajpatsankhla@gmail.com>
2024-06-28 21:18:29 +0500

    sring security dependency added

---
+++ src/main/java/study/course/course/configuration/SecurityConfig.java
@@ -0,0 +1,14 @@
+package study.course.course.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
+@Ena
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurereAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+    }
+}
