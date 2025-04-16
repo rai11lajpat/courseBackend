30c00741e1cd8a0c61b6a13cacf826a91e96324a
Lajpat Rai <lajpatsankhla@gmail.com>
2025-04-16 22:41:12 +0500

    dep dev added

--- src/main/java/study/course/course/configuration/SecurityConfig.java
+++ src/main/java/study/course/course/configuration/SecurityConfig.java
@@ -1,14 +1,26 @@
-package study.course.course.configuration;
+// package study.course.course.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@Ena
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurereAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-    }
-}
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception{
+//         http
+//         .csrf()
+//         .disable()
+//         .authorizeRequests()
+//         .anyRequest()
+//         .authenticated()
+//         .and()
+//         .httpBasic(); 
+
+//     }
+// }
